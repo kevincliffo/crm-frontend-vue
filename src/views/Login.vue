d14060244528dd116d8426a06c3ae38a5a64f713
Kevin Ochieng <kevincliffo@gmail.com>
2021-04-18 04:18:01 +0300

    working

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,19 +4,23 @@
             <div class="column is-4 is-offset-4">
                 <h1 class="title">Log In</h1>
 
-                <form action="" method="post">
+                <form @submit.prevent="submitForm">
                     <div class="field">
                         <label for="">Email</label>
                         <div class="control">
-                            <input type="email" name="email" id="email" class="input">
+                            <input v-model="username" type="email" name="email" id="email" class="input">
                         </div>
                     </div>
 
                     <div class="field">
                         <label for="">Password</label>
                         <div class="control">
-                            <input type="password" name="password" id="password" class="input">
+                            <input v-model="password" type="password" name="password" id="password" class="input">
                         </div>
+                    </div>
+
+                    <div v-if="errors.length" class="notification is-danger">
+                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
                     </div>
 
                     <div class="field">
@@ -32,8 +36,59 @@
     </div>
 </template>
 
-<script>
+<script>    
+    import axios from 'axios';
+    
     export default {
-        name:'Login'
+        name:'Login',
+        data(){
+            return {
+                username:'',
+                password:'',
+                errors:[]
+            }
+        },
+        methods:{
+            submitForm(){
+                axios.defaults.headers.common['Authorization'] = ''
+                localStorage.removeItem('token')
+                this.errors = []
+
+                if(this.username === ''){
+                    this.errors.push('The username is missing')
+                }
+                if(this.password === ''){
+                    this.errors.push('The password is too short')
+                }
+                if(!this.errors.length){
+                    const formData = {
+                        username: this.username,
+                        password:this.password,
+                    }
+                    axios
+                        .post('api/v1/token/login/', formData)
+                        .then(response =>{
+                            const token = response.data.auth_token
+
+                            this.$store.commit('setToken', token)
+
+                            axios.defaults.headers.common['Authorization'] = 'Token ' + token
+                            localStorage.setItem('token', token)
+
+                            this.$router.push('/dashboard/my-account')
+                        })
+                        .catch(error =>{
+                            if(error.response){
+                                for(const property in error.response.data){
+                                    this.errors.push(`${property}:${error.response.data[property]}`)
+                                }
+                            }
+                            else if (error.message){
+                                this.errors.push('Something went wrong, please try again')
+                            }
+                        })
+                }
+            }
+        }
     }
 </script>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,26 +4,30 @@
             <div class="column is-4 is-offset-4">
                 <h1 class="title">Sign Up</h1>
 
-                <form action="" method="post">
+                <form @submit.prevent="submitForm">
                     <div class="field">
                         <label for="">Email</label>
                         <div class="control">
-                            <input type="email" name="email" id="email" class="input">
+                            <input v-model="username" type="email" name="email" id="email" class="input">
                         </div>
                     </div>
 
                     <div class="field">
                         <label for="">Password</label>
                         <div class="control">
-                            <input type="password" name="password" id="password" class="input">
+                            <input v-model="password" type="password" name="password" id="password" class="input">
                         </div>
                     </div>
 
                     <div class="field">
                         <label for="">Repeat Password</label>
                         <div class="control">
-                            <input type="password" name="password2" id="password2" class="input">
+                            <input v-model="password2" type="password" name="password2" id="password2" class="input">
                         </div>
+                    </div>
+
+                    <div v-if="errors.length" class="notification is-danger">
+                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
                     </div>
 
                     <div class="field">
@@ -40,7 +44,62 @@
 </template>
 
 <script>
+    import axios from 'axios';
+    import {toast} from 'bulma-toast'
+
     export default {
-        name:'SignUp'
+        name:'SignUp',
+        data(){
+            return {
+                username:'',
+                password:'',
+                password1:'',
+                errors:[]
+            }
+        },
+        methods:{
+            submitForm(){
+                this.errors = []
+
+                if(this.username === ''){
+                    this.errors.push('The username is missing')
+                }
+                if(this.password === ''){
+                    this.errors.push('The password is too short')
+                }
+                if(this.password2 !== this.password){
+                    this.errors.push('The passwords are not matching')
+                }
+                if(!this.errors.length){
+                    const formData = {
+                        username: this.username,
+                        password:this.password,
+                    }
+                    axios
+                        .post('api/v1/users/', formData)
+                        .then(response =>{
+                            toast({
+                                message:'Account was created please log in',
+                                type:'is-success',
+                                dismissible:true,
+                                pauseOnHover:true,
+                                duration:2000,
+                                position:'bottom-right',
+                            })
+                            this.$router.push('/log-in')
+                        })
+                        .catch(error =>{
+                            if(error.response){
+                                for(const property in error.response.data){
+                                    this.errors.push(`${property}:${error.response.data[property]}`)
+                                }
+                            }
+                            else if (error.message){
+                                this.errors.push('Something went wrong, please try again')
+                            }
+                        })
+                }
+            }
+        }
     }
 </script>
